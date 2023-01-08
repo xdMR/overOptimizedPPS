d5c84052324560f6a08506f762eea1c5079a422f
Marko R <info@xdmr.us>
2023-01-08 14:29:33 -0500

    adds Slider for SliderTeam Componetn

--- sales_page/src/components/sliderteam/SliderTeam.jsx
+++ sales_page/src/components/sliderteam/SliderTeam.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import { useState } from 'react';
 import './sliderteam.css';
+import Slider  from 'rc-slider';
+
 
 function SliderTeam() {
+  const [sliderValue, setSliderValue] = useState(2);
+  const heightMarks = {
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+  };
+
   return (
-    <div>SliderTeam</div>
+    <>
+    <h1>{sliderValue}</h1>
+        <div className="slider" style={{width:"250px", height:"100px",display:"block"}}>
+        <Slider
+               id="slider"
+              defaultValue={2}
+              min={1}
+              max={7}
+              step={1}
+              style={{ width: "100%" }}
+              valueLabelDisplay="auto"
+              marks={heightMarks}
+              keyboard={true}
+              onChange={(value) => {
+                setSliderValue(value)
+              }}
+
+            />
+        </div>
+    </>
   )
 }
 
