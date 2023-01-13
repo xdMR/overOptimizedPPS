dc1ca1dea79d489dfecf58a56a99870a513f9f68
Marko R <info@xdmr.us>
2023-01-12 21:26:54 -0500

    cleans up Recordings component

--- sales_page/src/components/recordings/Recordings.jsx
+++ sales_page/src/components/recordings/Recordings.jsx
@@ -1,34 +1,31 @@
 import React from 'react'
-import { Button } from "../../components";
 import './recordings.css'
+import { Button } from "../../components";
 import { CartContext } from "../../context/CartContext";
 import { useContext } from "react";
 
 
 function Recordings() {
-    const cart = useContext(CartContext);
+  const cart = useContext(CartContext);
 
-    const plan2 = (priceId, planName, members) => {
-        console.log(`This is ${planName}`)
-        console.log(cart.cart);
-        cart.cart.teammembers=1;
-        cart.createOrder(priceId);
-      }
-
+  const pay = (priceId, planName) => {
+    console.log(`This is ${planName} plan`)
+    cart.cart.teammembers = 1;
+    cart.createOrder(priceId, 1);
+  }
 
   return (
     <div class="m__cta">
-    <div class="m__cta-content">
-        <h3>Recordings-Only Package</h3>
+      <div class="m__cta-content">
+        <h3>Recordings-Only Package - $97</h3>
         <p>9 months of access to the five intensive session recordings
-and notes (through Nov 6, 2023).</p>
+          and notes (through Nov 6, 2023).</p>
         <p>Catch the lectures on your own schedule.</p>
-
+      </div>
+      <div class="m__cta-btn">
+        <Button boring={false} onClick={() => { pay("price_1MPcuyCpotfJBdLxTsTD006j", "Recordings") }} title="Buy Recording Access" />
+      </div>
     </div>
-    <div class="m__cta-btn">
-    <Button boring={false} onClick={()=>{plan2("price_1MOueTCpotfJBdLx6CAptVdz","Recordings",1)}} title="Buy Recording Access" />
-    </div>
-</div>
   )
 }
 
